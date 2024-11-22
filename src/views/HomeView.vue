f93e3b8079dbc4715475b556cc030e22c19f63f6
magalutfullaev <mhoja9494@gmail.com>
2024-11-22 10:45:20 +0500

    feat: init project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <button>Open modal</button>
+  </main>
+</template>
+
+<style scoped lang="scss">
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+
+  button {
+    padding: 8px 25px;
+    border: none;
+    background: #0267c6;
+    color: #fff;
+  }
+}
+</style>
